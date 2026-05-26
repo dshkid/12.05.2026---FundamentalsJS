553681e86e41c3adeb221341ca521fd70884a2ab
Deyvid Halachev <dshkid@icloud.com>
2026-05-26 22:05:53 +0300

    updated-exercises

--- 03. Data Types and Variables/03. More Exercises/06. chess-board.js
+++ 03. Data Types and Variables/03. More Exercises/06. chess-board.js
@@ -1,5 +1,25 @@
 function chessBoard(n) {
 
+  let result = '<div class="chessboard">\n';
+
+  for (let row = 0; row < n; row++) {
+
+    result += ' <div>\n';
+
+    for (let col = 0; col < n; col++) {
+
+      if ((row + col) % 2 === 0) {
+        result += '   <span class="black"></span>\n';
+      } else {
+        result += '   <span class="white"></span>\n';
+      }
+
+    }
+    result += ' </div>\n';
+  }
+  result += '</div>';
+
+  console.log(result);
 }
 
 chessBoard(3);
